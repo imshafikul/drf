1d71c33f266a3892701b60b44123e1b0dd14adf9
Shafikul Islam <shafikul.me@gmail.com>
2019-07-17 15:48:21 +0600

    Vote url updated

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import poll_list, poll_details
 
-from .apiviews import PollList, PollDetails
+from .apiviews import PollList, PollDetails, ChoiceList, CreateVote
 
 
 urlpatterns = [
     path('polls', PollList.as_view(), name="poll_list"),
-    path('polls/<int:pk>/', PollDetails.as_view(), name="poll_details")
+    path('polls/<int:pk>/', PollDetails.as_view(), name="poll_details"),
+    path('polls/<int:pk>/choices', ChoiceList.as_view(), name="choice_list"),
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name="create_vote")
 ]
